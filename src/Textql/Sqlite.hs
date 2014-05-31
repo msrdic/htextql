e9718552e0c5ecc09d1b57c650c2ba0896a97f77
Mladen Srdic <m.s.qubits@gmail.com>
2014-05-31 16:40:44 +0000

    A bit more refactoring and generalising.

--- src/Textql/Sqlite.hs
+++ src/Textql/Sqlite.hs
@@ -3,51 +3,49 @@ module Textql.Sqlite where
 -- we need this to avoid qualified Data.Text
 import              Prelude hiding (concat)
 
-import              Data.Maybe
-import              Data.Text (Text, intercalate, concat, pack)
+import              Data.Text (Text, concat, pack)
 
 import              Database.Sqlite
 import              Database.Persist
 
 import              Textql.Types
+import              Textql.Utils
 
 type TableName = Text
 type ColumnName = Text
 
-joinWithSpace :: [Text] -> Text
-joinWithSpace = intercalate " "
-
-joinWithColon :: [Text] -> Text
-joinWithColon = intercalate ", "
-
 schemaQuery :: TableName -> [ColumnName] -> [Type] -> Text
-schemaQuery tableName columns types = concat [createTable, fieldDefs]
+schemaQuery tableName columns types = concat [createTable, columnsDef]
     where createTable = createTablePrefix tableName
-          fieldDefs = propperValues (joinWithColon $ map columnDef $ zip columns types)
-
-columnDef :: (ColumnName, Type) -> Text
-columnDef (colName, colType) = joinWithSpace [colName, pack $ show colType]
+          columnsDef = propperValues $ joinWithColon columnDefs
+          columnDefs = map columnDef columnsAndTypes
+          columnsAndTypes = zip columns types
 
 createTablePrefix :: Text -> Text
 createTablePrefix tableName = joinWithSpace ["CREATE TABLE", tableName]
 
+-- Make an Sqlite column definition. Used for constructing
+-- a create schema query.
+columnDef :: (ColumnName, Type) -> Text
+columnDef (colName, colType) = joinWithSpace [colName, pack $ show colType]
+
 insertQuery :: TableName -> [Text] -> Text
-insertQuery tableName values = joinWithSpace ["INSERT INTO", tableName, "VALUES", values']
-    where values' = propperValues $ joinWithColon (map quote values)
+insertQuery tableName values = joinWithSpace [insertPrefix tableName, "VALUES", values']
+    where values' = (propperValues . joinWithColon) quotedValues
+          quotedValues = map quote values
 
 batchInsertQuery :: TableName -> [[Text]] -> Text
-batchInsertQuery tableName values = joinWithSpace ["INSERT INTO", tableName, "VALUES", values', ";"]
+batchInsertQuery tableName values = joinWithSpace [insertPrefix tableName, "VALUES", values', ";"]
     where values'  = joinWithColon valueStrings
           valueStrings = map generateValueString values
+
+insertPrefix :: Text -> Text
+insertPrefix tableName = joinWithSpace ["INSERT INTO", tableName]
 
 -- take a list of record values and construct a single
 -- values part of an insert statement
 generateValueString :: [Text] -> Text
 generateValueString = batchValues . joinWithColon . (map quote)
-
-quote string = concat ["'", string, "'"]
-propperValues values = concat [batchValues values, ";"]
-batchValues values = concat ["(", values, ")"]
 
 withConnection :: Connection -> Text -> IO [PersistValue]
 withConnection connection query = do
--- src/Textql/Utils.hs
+++ src/Textql/Utils.hs
@@ -1,8 +1,18 @@
 module Textql.Utils where
 
-import Data.Text (Text, replace)
+import              Prelude hiding (concat)
+import              Data.Text (Text, replace, intercalate, concat)
 
 -- clean a line of text by removing delimiters
 clean :: Text -> Text -> Text
 clean string delim = replace delim "" string
 
+joinWithSpace :: [Text] -> Text
+joinWithSpace = intercalate " "
+
+joinWithColon :: [Text] -> Text
+joinWithColon = intercalate ", "
+
+quote string = concat ["'", string, "'"]
+propperValues values = concat [batchValues values, ";"]
+batchValues values = concat ["(", values, ")"]
